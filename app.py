9e0d695d7c8fed762e1bf5c292099dc882e842da
clcosta <clcostaf@hotmail.com>
2023-05-21 11:12:32 -0300

    change: app variables

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+import os
+
 from payment_webhook import app
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(
+        host=os.getenv('HOST', default='0.0.0.0'),
+        port=int(os.getenv('PORT', default=8000)),
+        debug=bool(os.getenv('DEBUG', default=False)),
+    )
